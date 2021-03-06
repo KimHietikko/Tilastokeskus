e44045375075eacabcef65675a2354afc4623ea2
Kim Hietikko <kim.hietikko@gmail.com>
2021-03-06 20:31:20 +0200

    swimlane charts

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [AppComponent, FrontPageComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- app/src/app/front-page/front-page.component.ts
+++ app/src/app/front-page/front-page.component.ts
@@ -7,6 +7,24 @@ import { ApiService } from '../api.service';
   styleUrls: ['./front-page.component.scss'],
 })
 export class FrontPageComponent implements OnInit {
+  multi: any[];
+  view: any[] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
+  };
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
